f5b632a72879e1ee05abe885cd9b7f26184cd7af
yoheikikuta <diracdiego@gmail.com>
2017-11-18 00:45:50 +0000

    Update notebook contents
    Also modify launch-notebook to allow root user.

--- launch-notebook.sh
+++ launch-notebook.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # USAGE: bash launch-notebook.sh (in the container)
 
-jupyter-notebook --ip=`ip route | awk 'NR==2 {print $9}'`
+jupyter-notebook --ip=`ip route | awk 'NR==2 {print $9}'` --allow-root
